d82746f0238645a1396f23af0cd345f5b2800edb
Dhaval Makwana <dhavalmak77@gmail.com>
2025-10-07 17:58:53 +0530

    connect mongodb

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,11 +1,21 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT;
 
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL as string);
+        console.log("Database connected");
+    } catch (error) {
+        console.log('Error connecting to mongodb database', error);
+    }
+};
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
+    connection();
 });
