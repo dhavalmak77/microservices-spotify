db0ccb23e59bc2ac6135dbb1d11ee28c1adac5f1
dhavalmak77 <dhavalmak77@gmail.com>
2025-10-08 23:58:59 +0530

    add redis in song service

--- song-service/dist/index.js
+++ song-service/dist/index.js
@@ -1,7 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import songRoutes from "./route.js";
+import redis from "redis";
 dotenv.config();
+export const redisClient = redis.createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT)
+    }
+});
 const app = express();
 const PORT = process.env.PORT;
 // Middleware
--- song-service/src/index.ts
+++ song-service/src/index.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import songRoutes from "./route.js";
+import redis from "redis";
 
 dotenv.config();
+
+export const redisClient = redis.createClient({
+	password: process.env.REDIS_PASSWORD as string,
+	socket: {
+		host: process.env.REDIS_HOST as string,
+		port: Number(process.env.REDIS_PORT as string)
+	}
+});
+
 const app = express();
 const PORT = process.env.PORT;
 
